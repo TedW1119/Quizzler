6d7704789714ca2df5153699292d83bb739afbb2
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2023-10-18 12:15:42 -0400

    Created reusable button composable

--- src/main/kotlin/composables/button.kt
+++ src/main/kotlin/composables/button.kt
@@ -2,20 +2,25 @@ package composables
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 
 @Composable
 @Preview
-fun helloWorldButton() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun button(text: String, filled: Boolean, action: () -> Unit) {
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        if (filled) {
+            Button(onClick = action) {
+                Text(text)
+            }
+        } else {
+            OutlinedButton(onClick = action) {
+                Text(text)
+            }
         }
     }
+
 }
--- src/main/kotlin/pages/landing.kt
+++ src/main/kotlin/pages/landing.kt
@@ -2,10 +2,14 @@ package pages
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-import composables.helloWorldButton
+import composables.button
 
 @Composable
 @Preview
 fun landing() {
-    helloWorldButton()
+    val input = "test input"
+    fun handleSubmit() {
+        println(input)
+    }
+    button("Test button", true, ::handleSubmit)
 }
