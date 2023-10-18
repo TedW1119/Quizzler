3a0fa1ffcbac9429c4255cc19be5eeefb222a9c2
Ted Weng <twks11192002@gmail.com>
2023-10-18 12:19:28 -0400

    created input field template

---
+++ src/main/kotlin/composables/input.kt
@@ -0,0 +1,23 @@
+package composables
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+@Composable
+@Preview
+fun input(label: String) {
+    val text = remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text.value,
+        singleLine = true,
+        onValueChange = { newText -> text.value = newText },
+        label = { Text(label) }
+    )
+}
