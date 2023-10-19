40042cfd9f186cf6b442f4489ec1b701d2ac2486
Gordon Ma <g27ma@uwaterloo.ca>
2023-10-19 19:42:14 -0400

    Quiz Creation UI

--- src/main/kotlin/pages/quiz-creation.kt
+++ src/main/kotlin/pages/quiz-creation.kt
@@ -1,9 +1,244 @@
 package pages
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 @Preview
 fun quizCreation() {
+
+    // Overall Box
+    Box(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.background(Color.White).padding(16.dp),
+
+        ) {
+            // Title Section
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.LightGray)
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Select Your Quizzer Style",
+                    fontSize = 32.sp,
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Selection Section
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    // Row #1
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(6.dp)
+                            .weight(1f)
+                    ) {
+                        // # of Questions Box
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .weight(1f)
+                                .background(Color.LightGray, shape = RoundedCornerShape(16.dp))
+                        ) {
+                            // Title of Box
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(2f)
+                                    .background(Color.Gray, shape = RoundedCornerShape(16.dp))
+                                    .padding(16.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    "# of Questions",
+                                    fontSize = 16.sp,
+                                )
+                            }
+                            // Options
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight(),
+                                verticalArrangement = Arrangement.Center,
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Button(onClick = {}) {
+                                        Text("5")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}) {
+                                        Text("10")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}) {
+                                        Text("20")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}) {
+                                        Text("?")
+                                    }
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.width(12.dp))
+                        // Difficulty Box
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .weight(1f)
+                                .background(Color.LightGray, shape = RoundedCornerShape(16.dp))
+                        ) {
+                            // Title of Box
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(2f)
+                                    .background(Color.Gray, shape = RoundedCornerShape(16.dp))
+                                    .padding(16.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    "Question Types",
+                                    fontSize = 16.sp,
+                                )
+                            }
+                            // Options
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight(),
+                                verticalArrangement = Arrangement.Center,
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Button(onClick = {}, modifier = Modifier.background(Color.Green)) {
+                                        Text("Easy")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}, modifier = Modifier.background(Color.Yellow)) {
+                                        Text("Medium")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}, modifier = Modifier.background(Color.Red)) {
+                                        Text("Hard")
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    // Row #2
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(6.dp)
+                            .weight(1f)
+                    ) {
+                        // Question Types Box
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .weight(1f)
+                                .background(Color.LightGray, shape = RoundedCornerShape(16.dp)),
+                        ) {
+                            // Title of Box
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(2f)
+                                    .background(Color.Gray, shape = RoundedCornerShape(16.dp))
+                                    .padding(16.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    "Question Types",
+                                    fontSize = 16.sp,
+                                )
+                            }
+                            // Options
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight(),
+                                verticalArrangement = Arrangement.Center,
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Button(onClick = {}) {
+                                        Text("MCQ")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}) {
+                                        Text("MSQ")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}) {
+                                        Text("T/F")
+                                    }
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                    Button(onClick = {}) {
+                                        Text("SA")
+                                    }
+                                }
+                            }
+                        }
+                        Spacer(modifier = Modifier.width(12.dp))
+                        // Extra Settings Box
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .weight(1f)
+                                .background(Color.LightGray, shape = RoundedCornerShape(16.dp))
+                        ) {
+                            // Title of Box
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(2f)
+                                    .background(Color.Gray, shape = RoundedCornerShape(16.dp))
+                                    .padding(16.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    "Extra Settings",
+                                    fontSize = 16.sp,
+                                )
+                            }
+                            // Options
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight(),
+                                verticalArrangement = Arrangement.Center,
+                            ) {}
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
