5b05681d2fa0c760d9a8ac2497a339550889108e
Myles Esteban <mesteban@uwaterloo.ca>
2023-11-30 21:45:05 -0500

    removed settings field from profile, replaced hamburger with dice to shuffle...

--- application/src/main/kotlin/composables/settingsOverlay.kt
+++ application/src/main/kotlin/composables/settingsOverlay.kt
@@ -2,12 +2,10 @@ package composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun settingsOverlay(
@@ -17,15 +15,13 @@ fun settingsOverlay(
     Column(
         modifier = Modifier
             .offset(0.dp, 56.dp)
-            .height(150.dp)
+            .height(130.dp)
             .width(150.dp)
             .background(Color.Gray.copy(alpha=0.98f))
             .padding(15.dp)
     ) {
-        // settings content, including menu items, can be added here
-        // Note: just text as placeholders for now, they will be buttons in the future
+        // settings content including menu items
         button("Profile", false, handleOpenAccountSettings)
-        Text("Settings", Modifier.padding(2.dp), fontSize = 15.sp)
         button("Logout", false, handleLogout)
     }
 }
