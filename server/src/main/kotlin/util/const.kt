bfaa1c882ec0a5f83dd12087d1bec96a075eb681
Ted Weng <twks11192002@gmail.com>
2023-12-26 15:11:18 -0500

    final update

--- server/src/main/kotlin/util/const.kt
+++ server/src/main/kotlin/util/const.kt
@@ -2,13 +2,13 @@ package util
 
 // if there is a better way of doing this plz do
 object Constants {
-    const val MONGO_URI = "mongodb+srv://abnormally:distributed@abnormally-distributed.naumhbd.mongodb.net/?retryWrites=true&w=majority"
+    const val MONGO_URI = "mongodb+srv://abnormally:distributedd@abnormally-distributed.naumhbd.mongodb.net/?retryWrites=true&w=majority"
     const val MONGO_DB = "abnormally-distributed"
     const val QUIZ_COLLECTION = "quizzes"
     const val QUESTION_COLLECTION = "questions"
     const val ACCOUNT_COLLECTION = "accounts"
     const val NOTE_COLLECTION = "notes"
-    const val OPENAI_TOKEN = "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"
+    const val OPENAI_TOKEN = "sk-fVVuikIcRakKlUrQKJL0T3BlbkFJU5GaP4tSrxPh2KGy0pBtt"
     const val OPENAI_MODEL_ID = "text-davinci-003"
     const val OPENAI_MAX_TOKENS = 600
     const val OPENAI_TEMPERATURE = 0.02
